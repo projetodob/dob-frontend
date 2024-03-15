60f8e454966f4e59ea331849e3db7706e606883a
gustavokarl <gustavoalrib@gmail.com>
2024-03-15 00:25:24 -0300

    implementada função de adicionar ao carrinho

--- src/components/produtos/listaProdutos/ListaProdutos.tsx
+++ src/components/produtos/listaProdutos/ListaProdutos.tsx
@@ -1,14 +1,18 @@
 import { useContext, useEffect, useState } from 'react';
-import { Dna } from 'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthContext';
 import Produto from '../../../models/Produto';
 import { buscar } from '../../../service/Service';
 import CardProduto from '../cardProdutos/CardProdutos';
 import { toastAlerta } from '../../../util/toastAlerta';
+import { CarrinhoContext } from '../../../contexts/CarrinhoContext';
 
 function ListaProdutos() {
+
   const [produtos, setProdutos] = useState<Produto[]>([]);
+
+  const { adicionarProdutoAoCarrinho } = useContext(CarrinhoContext);
 
   const navigate = useNavigate();
 
@@ -29,7 +33,6 @@ function ListaProdutos() {
           Authorization: token,
         },
       });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       if (error.toString().includes('403')) {
         toastAlerta('O token expirou, favor logar novamente', 'info')
@@ -41,21 +44,30 @@ function ListaProdutos() {
   useEffect(() => {
     buscarProdutos();
   }, [produtos.length]);
+
+  function adicionarAoCarrinho(produto: Produto) {
+    adicionarProdutoAoCarrinho(produto);
+  }
+
   return (
     <>
       {produtos.length === 0 && (
-        <Dna
-          visible={true}
-          height="200"
-          width="200"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper mx-auto"
-        />
+         <div className="text-center">
+         <ThreeDots
+           visible={true}
+           height="200"
+           width="200"
+           color="#7EAB88"
+           radius="9"
+           ariaLabel="three-dots-loading"
+           wrapperStyle={{ display: 'inline-block' }}
+           wrapperClass="mx-auto"
+         />
+       </div>
       )}
-      <div className='container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <div className='mt-8 container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {produtos.map((produto) => (
-          <CardProduto key={produto.id} prod={produto} />
+          <CardProduto key={produto.id} prod={produto} adicionarAoCarrinho={adicionarAoCarrinho} />
         ))}
       </div>
     </>
