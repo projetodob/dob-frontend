e17b2953aa68156b48764ed4e4942f74fe2eded8
gustavokarl <gustavoalrib@gmail.com>
2024-03-15 16:48:35 -0300

    alterações no carrinho

--- src/components/carrinho/CardCarrinho.tsx
+++ src/components/carrinho/CardCarrinho.tsx
@@ -56,7 +56,7 @@ function CardCarrinho({ produto }) {
                                     </span>
                                 </div>
                                 <div className="flex items-center space-x-4">
-                                    <p className="text-sm">R$ {produto.preco}</p>
+                                    <p className="text-sm">R$ {produto.preco.toFixed(2)}</p>
                                     <svg onClick={() => handleClickRemover(produto.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
