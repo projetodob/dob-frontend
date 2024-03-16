b86e18d4a9e0cdd1fac9c6fcd373ef4175500c8e
gustavokarl <gustavoalrib@gmail.com>
2024-03-16 03:48:11 -0300

    icones estão funcionais

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,29 +1,38 @@
-import { Envelope, GithubLogo } from '@phosphor-icons/react'
-
+import { Envelope, GithubLogo } from '@phosphor-icons/react';
 
 function Footer() {
- 
-  
+
+  const handleButtonClick = () => {
+    window.open('https://github.com/projetodob');
+  };
 
   return (
     <>
-        <div className="flex justify-center bg-laranjaEsc text-white">
-          <div className="container flex flex-col items-center py-4">
+      <div className="flex justify-center bg-laranjaEsc text-white">
+        <div className="container flex flex-col items-center py-4">
           <div className="p-4 text-center">
             <p className='text-white font-bold'>© 2024 Copyright: Desenvolvendo o Bem</p>
+          </div>
+          <div className='mb-2 flex justify-center w-full'>
+            <div className='mr-3 cursor-pointer'>
+              <GithubLogo
+                size={48}
+                weight='light'
+                onClick={handleButtonClick} />
             </div>
-            <div className='mb-2 flex justify-center w-full'>
-              <div className='mr-3'>
-              <GithubLogo size={48} weight='light' />
-              </div>
-              <div className='ml-3'>
-              <Envelope size={48} weight='light' />
-              </div>
+            <div className='ml-3'>
+              <a href="mailto:projetointegradordob@gmail.com">
+                <Envelope
+                  size={48}
+                  weight='light'
+                />
+              </a>
             </div>
           </div>
-        </div> 
-      </>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Footer
+export default Footer;
